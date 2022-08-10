1170b1580428fe1f7696b1fc8126be5be9af7be4
DoubangoTelecom <github@doubango.org>
2022-08-11 00:16:04 +0200

    Update python_deepfake.bat

--- binaries/windows/x86_64/python_deepfake.bat
+++ binaries/windows/x86_64/python_deepfake.bat
@@ -3,6 +3,5 @@ set PYTHONPATH=%PYTHONPATH%;.;../../../python
 set PATH=%PATH%;%~dp0
 python ../../../samples/python/deepfake/deepfake.py ^
 	--video ../../../assets/videos/deepfake.mp4 ^
-	--assets ../../../assets ^
-	--tokenfile C:/Projects/GitHub/ultimate/FaceLivenessDetection/SDK_dev/tokens/windows-iMac.lic
+	--assets ../../../assets
 endlocal
